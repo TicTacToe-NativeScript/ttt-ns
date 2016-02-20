7b7849009454b9a353ee0c76123fc0656734aa47
Pip3r4o <pip3r4o@gmail.com>
2016-02-20 23:31:23 +0200

    added game session placeholder

--- app/views/game/game-page.js
+++ app/views/game/game-page.js
@@ -9,7 +9,69 @@ function pageLoaded(args) {
 function pageNavigatedTo(args) {
 }
 
+function disableBoard() {
+    
+}
+
+function removeCellEvent() {
+    
+}
+
+function tapCell0() {
+    let result = viewModel.placeMark(0);
+    // get result
+    // if move was valid ->
+        // won?
+            // dialog -> redirect to home (without back functionality)
+        // switch active player's turn
+            // if not current player's turn -> disableBoard?
+        // start ticking down timer
+        // redrawBoard
+        // remove event listener of this element
+}
+
+function tapCell1() {
+    let result = viewModel.placeMark(1);
+}
+
+function tapCell2() {
+    let result = viewModel.placeMark(2);
+}
+
+function tapCell3() {
+    let result = viewModel.placeMark(3);
+}
+
+function tapCell4() {
+    let result = viewModel.placeMark(4);
+}
+
+function tapCell5() {
+    let result = viewModel.placeMark(5);
+}
+
+function tapCell6() {
+    let result = viewModel.placeMark(6);
+}
+
+function tapCell7() {
+    let result = viewModel.placeMark(7);
+}
+
+function tapCell8() {
+    let result = viewModel.placeMark(8);
+}
+
 module.exports = {
   pageLoaded,
-  pageNavigatedTo
+  pageNavigatedTo,
+  tapCell0,
+  tapCell1,
+  tapCell2,
+  tapCell3,
+  tapCell4,
+  tapCell5,
+  tapCell6,
+  tapCell7,
+  tapCell8
 };
--- app/views/game/game-view-model.js
+++ app/views/game/game-view-model.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let Observable = require('data/observable').Observable;
+let marks = [' ', 'X', 'O'];
 
 class GameViewModel extends Observable {
   constructor() {
@@ -12,6 +13,34 @@ class GameViewModel extends Observable {
     this.secondPlayer = {
       userName: 'Second Player'
     };
+    
+    this.board = {};
+    this.isPlayerOneTurn = true;
+  }
+  
+  checkStatus() {
+      // Get IsPlayer1 from db
+      // -> set to property
+      // -> return
+      
+      return this.isPlayerOneTurn;
+  }
+  
+  getBoard() {
+      // rebind all cells in this.board
+  }
+  
+  placeMark(pos) {
+      // Check if position is available
+      // Check which player's turn it is
+      
+      // Place marker on board depending on this.isPlayerOneTurn
+      // save board on db
+      // check if win condition is met
+      
+      // this.isPlayerOneTurn = !this.isPlayerOneTurn
+      // restart timer
+      console.log(pos + " cell tapped");
   }
 }
 
