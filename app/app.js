6ac21f489c1e5fa2f829854cc3e3dae9276f6e69
Pip3r4o <pip3r4o@gmail.com>
2016-02-20 13:01:27 +0200

    restored app.js

--- app/app.js
+++ app/app.js
@@ -1,2 +1,19 @@
 var application = require("application");
-application.start({ moduleName: "main-page" });
+
+/*
+ * Globals and constants
+*/
+
+global.BS_API_KEY = "46mdgkw9d134t4ao";
+global.BS_URL = "";
+global.TOKEN_DATA_KEY = "authenticationToken";
+global.EVERLIVE = require('~/lib/everlive.all.min.js');
+
+/*
+ * Application setup
+*/
+
+application.mainModule = "./views/main/main";
+application.cssFile = "./styles/app.css";
+
+application.start();
