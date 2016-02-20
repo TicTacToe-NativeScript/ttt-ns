6383f412ea865e061db225caf8732ae250a9b073
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-02-20 17:40:22 +0200

    Added view model for browse games, listed mocked game objects.

---
+++ app/views/browser/browser-page.js
@@ -0,0 +1,20 @@
+'use strict';
+let nativescriptEffects = require('nativescript-effects');
+let viewModel = require('./browser-view-model').browserViewModel;
+
+function pageLoaded(args) {
+  let page = args.object;
+  page.bindingContext = viewModel;
+}
+
+function onItemLoading(args) {
+  let item = args.object;
+  // item.opacity = 0;
+  // item.floatIn(700, 'right');
+}
+
+
+module.exports = {
+  pageLoaded,
+  onItemLoading
+};
